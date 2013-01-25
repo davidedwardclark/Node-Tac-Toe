c44ddcc488a47793442349d0563c8279f16b59f1
David Clark <davidedwardclark@gmail.com>
2013-01-25 00:05:31 -0800

    Refactored the code, made a startGame function, moved stuff around.

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
     var gameOn = true;
     var whoseTurn = 1;
     var history = [];
+    var player1 = [];
+    var player2 = [];
     var blue = '#0033ff';
+    var board = document.getElementById('board');
+    var squares = board.getElementsByTagName('td');
     
-    // Add click events to board squares
+    // Add click events to squares & fire checkSquare if someone clicks one
     var initialiseBoard = function(func) {
-        var board = document.getElementById('board');
-        var squares = board.getElementsByTagName('td');
         for (var i = 0; i < squares.length; i++) {
             squares[i].addEventListener('click', function() {
                 func(this);
@@ -27,12 +29,13 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
     };
     
     // Check to see if someone has already selected that square
-    // If they haven't then fire the rest of the functions
+    // If they haven't then update the history & check the win state
     var checkSquare = function(elem) {
         var value = elem.innerHTML;
-        if ((value !== 'X') && (value !== 'O') && gameOn) {
+        if (gameOn && (value !== 'X') && (value !== 'O') ) {
             updateHistory(elem);
-            checkWin(elem);
+            selectSquare(elem);
+            checkWin();
         }
     };
     
@@ -42,56 +45,41 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
         var square = Number(elem.getAttribute('class').replace('tile-', ''));
         var turn = { 'player' : player, 'square' : square };
         history.push(turn);
+        if (player === 1) {
+            player1.push(square);
+        } else {
+            player2.push(square);
+        }
     };
     
     // Check if someone has won
-    var checkWin = function(elem) {
-        var player1 = [];
-        var player2 = [];
-        var winningCombinations = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3] ];
+    var checkWin = function() {
         var winningCombo;
-        
-        for (var i = 0; i < history.length; i++) {
-            if (history[i].player === 1) {
-                player1.push(history[i].square);
-            } else if (history[i].player === 2) {
-                player2.push(history[i].square);
-            }
-        }
-        
-        if (player1.length >= 3) {
-            var player1String = player1.join('');
-            var player2String = player2.join('');
-            for (var j = 0; j < winningCombinations.length; j++) {
-                var combo = winningCombinations[j].join('');
-                var regex = new RegExp('\[' + combo + '\]', 'g');
-                var player1Match = player1String.match(regex);
-                if ((player1Match === null) || (player1Match === undefined)) { player1Match = []; }
-                var player2Match = player2String.match(regex);
-                if ((player2Match === null) || (player2Match === undefined)) { player2Match = []; }
-                if (player1Match.length === 3) { winningCombo = player1Match; }
-                if (player2Match.length === 3) { winningCombo = player2Match; }
-                if ((player1Match.length === 3) || (player2Match.length === 3)) {
-                    gameOn = false;
-                    player1 = [];
-                    player2 = [];
-                    selectSquare(elem);
-                    endGame(winningCombo, 'win');
-                    break;
-                }
+        var winningCombinations = [
+            [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+            [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]
+        ];
+        var player1String = player1.join('');
+        var player2String = player2.join('');
+        for (var j = 0; j < winningCombinations.length; j++) {
+            var combo = winningCombinations[j].join('');
+            var regex = new RegExp('[' + combo + ']', 'g');
+            var player1Match = player1String.match(regex);
+            if ((player1Match === null) || (player1Match === undefined)) { player1Match = []; }
+            var player2Match = player2String.match(regex);
+            if ((player2Match === null) || (player2Match === undefined)) { player2Match = []; }
+            if (player1Match.length === 3) { winningCombo = player1Match; }
+            if (player2Match.length === 3) { winningCombo = player2Match; }
+            if ((player1Match.length === 3) || (player2Match.length === 3)) {
+                endGame(winningCombo, 'win');
+                return;
             }
         }
         if ((history.length === 9) && gameOn) {
-            gameOn = false;
-            player1 = [];
-            player2 = [];
-            winningCombo = [];
-            selectSquare(elem);
-            updateStatus('Tie');
             endGame(winningCombo, 'tie');
+            return;
         }
         if (gameOn) {
-            selectSquare(elem);
             changeTurn();
         }
     };
@@ -124,15 +112,14 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
     
     // End the game and reset everything
     var endGame = function(winningCombo, status) {
-        if (status === 'win') {
-            var winner = 'Player ' + whoseTurn;
-            updateStatus(winner + ' Wins');
-        }
-        var board = document.getElementById('board');
+        var winner = 'Player ' + whoseTurn;
+        var highlight = [];
+        gameOn = false;
+        player1 = [];
+        player2 = [];
         board.setAttribute('class','quiet');
         if (status === 'win') {
-            var highlight = [];
-            var squares = board.getElementsByTagName('td');
+            updateStatus(winner + ' Wins');
             for (var i = 0; i < 3; i++) {
                 highlight[i] = 'tile-' + winningCombo.shift();
                 for (var j = 0; j < squares.length; j++) {
@@ -143,14 +130,23 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
                 }
             }
         }
+        if (status === 'tie') {
+            winningCombo = [];
+            updateStatus('Tie');
+        }
+        startGame();
+    };
+
+    // Start a new game.
+    var startGame = function() {
         var newgame = document.getElementById('newgame');
         newgame.setAttribute('class','show');
         newgame.addEventListener('click', function() {
             gameOn = true;
+            history = [];
+            whoseTurn = 1;
             board.setAttribute('class','');
             updateStatus('Player 1&rsquo;<sup>s</sup> Turn');
-            whoseTurn = 1;
-            history = [];
             var squares = board.getElementsByTagName('td');
             for (var i = 0; i < squares.length; i++) {
                 squares[i].innerHTML = '&nbsp;';
@@ -159,9 +155,7 @@ Notes: JQuery is for wimps. Strict is for ballers. Nuff said.
             this.setAttribute('class','hide');
         }, false);
     };
-
     
     // Start the game
     initialiseBoard(checkSquare);
-      
 }());
