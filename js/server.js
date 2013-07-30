f7ff41726db0063197f3d8f87cb0311961bcb9d3
David Clark <davidedwardclark@gmail.com>
2013-07-30 11:11:39 -0700

    Added mongoose starter code. Can now create, save and find MongoDB documents.

--- js/server.js
+++ js/server.js
@@ -1,70 +1,64 @@
 // NodeJS to MongoDB Connection via MongooseJS
 
-var DATABASE = 'Node-Tac-Toe';
 var mongoose = require('mongoose');
+var DATABASE = 'Node-Tac-Toe';
+
 mongoose.connect('mongodb://localhost/' + DATABASE);
+var databaseConnection = mongoose.connection;
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', function callback() {
+databaseConnection.on('error', console.error.bind(console, 'Connection error:'));
+databaseConnection.once('open', function callback() {
     console.log('Connected to ' + DATABASE + '.');
+
+    // Schemas
+    var nttGamesSchema = mongoose.Schema({
+        date: { type: Date, default: Date.now },    // Date of game creation.
+        player1Ip: String,                          // IP address of the first player.
+        player2Ip: String                           // IP address of the second player.
+    });
+    var nttGames = mongoose.model('nttGames', nttGamesSchema);
+    var nttMovesSchema = mongoose.Schema({
+        player: Number,                             // Player 1 or player 2.
+        square: Number,                             // Square they placed on.
+        gameId: Object                              // Game id.
+    });
+    var nttMoves = mongoose.model('nttMoves', nttMovesSchema);
+
+    // Create
+    var gameDocument = new nttGames({ player1Ip: '64.251.74.162', player2Ip: '64.251.74.162'});
+    var moveDocument = new nttMoves({ player: 1, position: 3, gameId: gameDocument._id});
+
+    // Save
+    gameDocument.save(function (err, gameDocument) {
+        if (err) {
+            console.log("Save error: " + err);
+        } else {
+            console.log("Game document saved. Id: " + gameDocument._id);
+        }
+    });
+    moveDocument.save(function (err, moveDocument) {
+        if (err) {
+            console.log("Save error: " + err);
+        } else {
+            console.log("Move document saved. Id: " + moveDocument._id);
+        }
+    });
+
+    // Find
+    nttGames.find(function (err, gameDocument) {
+        if (err) {
+            console.log("Find error: " + err);
+        } else {
+            console.log("Game document found:")
+            console.log(gameDocument);
+        }
+    });
+    nttMoves.find(function (err, moveDocument) {
+        if (err) {
+            console.log("Find error: " + err);
+        } else {
+            console.log("Move document found:")
+            console.log(moveDocument);
+        }
+    });
 });
-
-var NTT_Games = new Schema({
-    date: { type: Date, default: Date.now },    // Date of game creation.
-    p1Ip: String,                               // IP address of the first player.
-    p2Ip: String                                // IP address of the second player.
-});
-
-var NTT_Moves = new Schema({
-    date: { type: Date, default: Date.now },    // Date of move.
-    player: String,                             // Player 1 or player 2?
-    position: Number                            // Where did they place
-});
-
-var blogSchema = new Schema({
-  title:  String,
-  author: String,
-  body:   String,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs:  Number
-  }
-});
-
-var kittySchema = mongoose.Schema({
-    name: String
-});
-
-kittySchema.methods.speak = function () {
-    var greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
-    console.log(greeting);
-};
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name);
-
-var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak();
-
-fluffy.save(function (err, fluffy) {
-    if (err) {
-        console.log(err);
-    }
-    fluffy.speak();
-});
-
-Kitten.find(function (err, kittens) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(kittens);
-});
-
-// Kitten.find({ name: /^Fluff/ }, callback);
-
