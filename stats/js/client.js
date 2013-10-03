ba7232ef50ea82f90a68cd22b09a4f40e0b13938
David Clark <davidedwardclark@gmail.com>
2013-10-02 23:58:18 -0700

    got stats working reset: true when fetching ftw

--- stats/js/client.js
+++ stats/js/client.js
@@ -1,71 +1,70 @@
 (function () {
 
-	Game = Backbone.Model.extend({});
+    Game = Backbone.Model.extend({});
 
-	Games = Backbone.Collection.extend({
-		model: Game,
-		url: '/stats/data/'
-	});
+    Games = Backbone.Collection.extend({
+        model: Game,
+        url: '/stats/data/'
+    });
 
-	gameSet = new Games();
+    gameSet = new Games();
 
-	GameView = Backbone.View.extend({
-		tagName: 'div',
-		className: 'game',
-		initialize: function () {
-			this.model.bind('change', this.render, this);
-			this.template = _.template($('#game-template').html());
-		},
-		render: function () {
-			var renderedContent = this.template(this.model.toJSON());
-			$(this.el).html(renderedContent);
-			return this;
-		}
-	});
+    GameView = Backbone.View.extend({
+        tagName: 'li',
+        initialize: function () {
+            this.model.bind('change', this.render, this);
+            this.template = _.template($('#game-template').html());
+        },
+        render: function () {
+            var renderedContent = this.template(this.model.toJSON());
+            $(this.el).html(renderedContent);
+            return this;
+        }
+    });
 
-	SetGameView = GameView.extend({});
+    SetGameView = GameView.extend({});
 
-	SetView = Backbone.View.extend({
-		tagName: 'section',
-		initialize: function () {
-			this.collection.bind('reset', this.render, this);
-			this.template = _.template($('#set-template').html());
-		},
-		render: function () {
-			var $games;
-			var collection = this.collection;
-			$(this.el).html(this.template({}));
-			$games = this.$('.games');
-			collection.each(function (game) {
-				var view = new SetGameView({
-					model: game,
-					collection: collection
-				});
-				$games.append(view.render().el);
-			});
-			return this;
-		}
-	});
+    SetView = Backbone.View.extend({
+        tagName: 'section',
+        initialize: function () {
+            this.collection.bind('reset', this.render, this);
+            this.template = _.template($('#set-template').html());
+        },
+        render: function () {
+            var $games;
+            var collection = this.collection;
+            $(this.el).html(this.template({}));
+            $games = this.$('.games');
+            collection.each(function (game) {
+                var view = new SetGameView({
+                    model: game,
+                    collection: collection
+                });
+                $games.append(view.render().el);
+            });
+            return this;
+        }
+    });
 
-	NTTStats = Backbone.Router.extend({
-		routes: {
-			'': 'home'
-		},
-		initialize: function () {
-			this.setView = new SetView({
-				collection: gameSet
-			});
-		},
-		home: function () {
-			var $container = $('.container');
-			$container.empty();
-			$container.append(this.setView.render().el);
-		}
-	});
+    NTTStats = Backbone.Router.extend({
+        routes: {
+            'stats/': 'home'
+        },
+        initialize: function () {
+            this.setView = new SetView({
+                collection: gameSet
+            });
+        },
+        home: function () {
+            var $container = $('.container');
+            $container.empty();
+            $container.append(this.setView.render().el);
+        }
+    });
 
-	$(document).ready(function () {
-		App = new NTTStats();
-		Backbone.history.start({ pushState: true });
-	});
+    $(document).ready(function () {
+        App = new NTTStats();
+        Backbone.history.start({ pushState: true });
+    });
 
 })();
