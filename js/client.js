78a966a6fa390a1b9d06a7a917595ed5292fe0d3
David Clark <davidedwardclark@gmail.com>
2014-01-05 23:28:41 -0800

    completed multiplayer mode

--- js/client.js
+++ js/client.js
@@ -7,20 +7,33 @@ Author: David Clark
 
 (function () {
 
-    "use strict";
+    'use strict';
 
     var socket = io.connect('http://localhost:8080');
+    var urlLocation = window.location.href;
     var room = window.location.pathname.split('/').pop();
     var player;
-    var gameOn = false;
-    var whoseTurn = 1;
-    var history = [];
-    var player1 = [];
-    var player2 = [];
+    var whoseTurn;
+    var myTurn;
+    var gameStarted;
+    var winStatus = false;
+    var winningCombo;
     var blue = '#0033ff';
+    var body = document.getElementsByTagName('body')[0];
+    var shareScreenURL = document.getElementById('shareScreenURL');
+    var messageContainer = document.getElementById('message');
     var board = document.getElementById('board');
     var squares = board.getElementsByTagName('td');
-    var newGame = document.getElementById('newgame');
+    var player1History = [];
+    var player2History = [];
+    var player1String;
+    var player2String;
+    var winningCombinations = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+        [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]
+    ];
+    var winningComboCheck;
+    var whoWon;
 
     // Once connected emit which room we want to connect to.
     socket.on('connect', function () {
@@ -33,81 +46,83 @@ Author: David Clark
         if (player === 1 || player === 2) {
             console.log('This is player ' + player + '.');
         }
+        if (player === 1) {
+            shareScreen(true);
+        }
+        if (player === 2) {
+            updatePlayer('Player 2');
+            shareScreen(false);
+        }
         if (player === 3) {
+            updatePlayer('Observer');
+            socket.emit('observerHistoryRequest', true);
             console.log('This is an observer.');
         }
     });
 
-    // Receive socket data which confirms that the server recorded the move
-    socket.on('update', function (data) {
-        console.log(data);
+    // Find out when the game has started.
+    socket.on('gameStarted', function (data) {
+        gameStarted = data;
+        if (gameStarted === true) {
+            if (player === 1) {
+                shareScreen(false);
+            }
+            updateStatus('Your Turn');
+            console.log('The game has started.');
+        }
     });
 
-    // New game event
-    newGame.addEventListener('click', function () {
-        startGame();
-    }, false);
-
-    // Board square events
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', function () {
-            checkSquare(this);
-        }, false);
-    }
-
-    // Start a new game
-    var startGame = function () {
-        gameOn = true;
-        whoseTurn = 1;
-        history = [];
-        player1 = [];
-        player2 = [];
-        board.setAttribute('class', '');
-        updateStatus('Player 1&rsquo;<sup>s</sup> Turn');
-        for (var i = 0; i < squares.length; i++) {
-            squares[i].innerHTML = '&nbsp;';
-            squares[i].setAttribute('style', '');
+    // Find out whose turn it is.
+    socket.on('whoseTurn', function (data) {
+        var statusMessage;
+        whoseTurn = data;
+        if (!winStatus) {
+            if (whoseTurn === player) {
+                myTurn = true;
+                statusMessage = 'Your Turn';
+                console.log('It is my turn.');
+            } else {
+                myTurn = false;
+                statusMessage = 'Player ' + whoseTurn + '&rsquo;<sup>s</sup> Turn';
+                console.log('It is player ' + whoseTurn + '\'s turn.');
+            }
+            updateStatus(statusMessage);
         }
-    };
+    });
 
-    // Check to see if someone has already selected that square
-    // If they haven't then update the history & check the win state
-    var checkSquare = function (elem) {
-        var value = elem.innerHTML;
-        if (gameOn && (value !== 'X') && (value !== 'O')) {
-            selectSquare(elem);
-            updateHistory(elem);
-            checkWin();
-            playSound('click');
+    // Find out where the other player went and place that piece on the board.
+    socket.on('update', function (data) {
+        var pieceType;
+        if (!myTurn) {
+            if (data.player === 1) {
+                pieceType = 'X';
+            } else {
+                pieceType = 'O';
+            }
+            for (var i = 0; i < squares.length; i++) {
+                if (Number(squares[i].getAttribute('class').replace('tile-', '')) === data.square) {
+                    squares[i].innerHTML = pieceType;
+                }
+            }
+            console.log(data);
         }
-    };
+    });
 
-    // Update the history and send the results to the server
-    var updateHistory = function (elem) {
-        var player = whoseTurn;
-        var square = Number(elem.getAttribute('class').replace('tile-', ''));
-        var turn = { 'player' : player, 'square' : square };
-        history.push(turn);
-        if (player === 1) {
-            player1.push(square);
-        } else {
-            player2.push(square);
+    // Find out the game history and populate the observers game board with it.
+    // We need to check to see if the game has finished yet.
+    socket.on('gameHistory', function (data) {
+        var gameHistory = data;
+        var playerTurn;
+        shareScreen(false);
+        for (var i = 0; i < gameHistory.length; i++) {
+            if (isEven(i)) {
+                player1History.push(gameHistory[i]);
+            } else {
+                player2History.push(gameHistory[i]);
+            }
         }
-        socket.emit('move', {
-            player: whoseTurn,
-            square: square
-        });
-    };
-
-    // Check if someone has won
-    var checkWin = function () {
-        var winningCombo;
-        var winningCombinations = [
-            [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
-            [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]
-        ];
-        var player1String = player1.join('');
-        var player2String = player2.join('');
+        player1String = player1History.join('');
+        player2String = player2History.join('');
         for (var j = 0; j < winningCombinations.length; j++) {
             var combo = winningCombinations[j].join('');
             var regex = new RegExp('[' + combo + ']', 'g');
@@ -115,23 +130,84 @@ Author: David Clark
             if ((player1Match === null) || (player1Match === undefined)) { player1Match = []; }
             var player2Match = player2String.match(regex);
             if ((player2Match === null) || (player2Match === undefined)) { player2Match = []; }
-            if (player1Match.length === 3) { winningCombo = player1Match; }
-            if (player2Match.length === 3) { winningCombo = player2Match; }
+            if (player1Match.length === 3) {
+               winningComboCheck = player1Match;
+               whoWon = 1;
+            }
+            if (player2Match.length === 3) {
+                winningComboCheck = player2Match;
+                whoWon = 2;
+            }
             if ((player1Match.length === 3) || (player2Match.length === 3)) {
-                endGame('win', winningCombo);
+                updateStatus('Player ' + whoWon + ' won');
+                applyGameHistory(gameHistory);
+                winningComboHighlight(winningComboCheck);
+                board.setAttribute('class', 'quiet');
                 return;
+            } else {
+                if (isEven(data.length)) {
+                    playerTurn = 1;
+                } else {
+                    playerTurn = 2;
+                }
+                updateStatus('Player ' + playerTurn + '&rsquo;<sup>s</sup> Turn');
             }
         }
-        if ((history.length === 9) && gameOn) {
-            endGame('tie');
-            return;
+        if ( (!whoWon) && (gameHistory.length === 9) ) {
+            updateStatus('Tie');
+            applyGameHistory(gameHistory);
+            board.setAttribute('class', 'quiet');
         }
-        if (gameOn) {
-            changeTurn();
+        if ( (!whoWon) && (gameHistory.length < 9) ) {
+            applyGameHistory(gameHistory);
+        }
+    });
+
+    // Find out if anyone has won the game or if it is a tie game.
+    socket.on('winStatus', function (data) {
+        winStatus = true;
+        if ( (data.player === 1) || (data.player === 2) ) {
+            updateStatus('Player ' + data.player + ' Wins');
+            board.setAttribute('class', 'quiet');
+            winningComboHighlight(data.winningCombo);
+            console.log('Player ' + data.player + ' won.');
+        }
+        if (data === 'tie') {
+            board.setAttribute('class', 'quiet');
+            updateStatus('Tie');
+            console.log('Tie game.');
+        }
+    });
+
+    // Board square events.
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', function () {
+            checkSquare(this);
+        }, false);
+    }
+
+    // Check to see if the game has started, that it is the correct players turn
+    // and that no one has already gone on that piece yet.
+    // If so then select the square and emit the move.
+    var checkSquare = function (elem) {
+        var value = elem.innerHTML;
+        if (gameStarted && !winStatus && myTurn && (value !== 'X') && (value !== 'O')) {
+            selectSquare(elem);
+            emitMove(elem);
+            playSound('click');
         }
     };
 
-    // Mark the square as selected
+    // Emit the move to the server.
+    var emitMove = function (elem) {
+        var square = Number(elem.getAttribute('class').replace('tile-', ''));
+        socket.emit('move', {
+            player: player,
+            square: square
+        });
+    };
+
+    // Mark the square as selected.
     var selectSquare = function (elem) {
         if (whoseTurn === 1) {
             elem.innerHTML = 'X';
@@ -140,48 +216,75 @@ Author: David Clark
         }
     };
 
-    // Update the message board
+    // Update the message board.
     var updateStatus = function (value) {
-        var container = document.getElementById('message');
+        messageContainer.innerHTML = value;
+    };
+
+    // Update the player.
+    var updatePlayer = function (value) {
+        var container = document.getElementById('player');
         container.innerHTML = value;
     };
 
-    // Update the message to let the user know whose turn it is
-    var changeTurn = function () {
-        if (whoseTurn === 1) {
-            whoseTurn = 2;
-            updateStatus('Player 2&rsquo;<sup>s</sup> Turn');
-        } else {
-            whoseTurn = 1;
-            updateStatus('Player 1&rsquo;<sup>s</sup> Turn');
-        }
-    };
-
-    // End the game and reset everything
-    var endGame = function (status, winningCombo) {
-        var winner = 'Player ' + whoseTurn;
-        var highlight = [];
-        gameOn = false;
-        board.setAttribute('class', 'quiet');
-        if (status === 'win') {
-            playSound('endgame');
-            updateStatus(winner + ' Wins');
-            for (var i = 0; i < 3; i++) {
-                highlight[i] = 'tile-' + winningCombo.shift();
-                for (var j = 0; j < squares.length; j++) {
-                    var currClass = squares[j].getAttribute('class');
-                    if (currClass === highlight[i]) {
-                        squares[j].style.color = blue;
+    // Apply game history to the board.
+    var applyGameHistory = function (value) {
+        var movesMade = value;
+        var pieceType;
+        for (var i = 0; i < movesMade.length; i++) {
+            for (var j = 0; j < squares.length; j++) {
+                if (movesMade[i] === (j + 1)) {
+                    if (isEven(i)) {
+                        pieceType = 'X';
+                    } else {
+                        pieceType = 'O';
                     }
+                    squares[j].innerHTML = pieceType;
                 }
             }
-        } else if (status === 'tie') {
-            winningCombo = [];
-            updateStatus('Tie');
         }
     };
 
-    // Play sounds
+    // Check if a number is odd or even.
+    var isEven = function (value) {
+        if (value%2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    // Apply highlight to winning combo.
+    var winningComboHighlight = function (value) {
+        var winningCombo = value;
+        var highlight = [];
+        var currClass;
+        for (var i = 0; i < 3; i++) {
+            highlight[i] = 'tile-' + winningCombo.shift();
+            for (var j = 0; j < squares.length; j++) {
+                currClass = squares[j].getAttribute('class');
+                if (currClass === highlight[i]) {
+                    squares[j].style.color = blue;
+                }
+            }
+        }
+    };
+
+    // Share screen.
+    var shareScreen = function (value) {
+        if (value === true) {
+            console.log(urlLocation);
+            body.setAttribute('class', 'sharescreen');
+            shareScreenURL.innerHTML = urlLocation;
+        }
+        if (value === false) {
+            body.setAttribute('class', '');
+            board.setAttribute('class', 'show');
+            messageContainer.setAttribute('class', 'show');
+        }
+    };
+
+    // Play sounds.
     var playSound = function (audio) {
         var audio5js = new Audio5js({
             swf_path: '/libraries/audio5js.swf',
@@ -190,15 +293,10 @@ Author: David Clark
             ready: function () {
                 if (audio === 'click') {
                     this.load('/audio/click.mp3');
-                } else if (audio === 'endgame') {
-                    this.load('/audio/endgame.mp3');
                 }
                 this.play();
             }
         });
     };
-
-    // Start the game
-    startGame();
 
 })();
--- server.js
+++ server.js
@@ -7,16 +7,21 @@ Author: David Clark
 
 (function () {
 
-    "use strict";
+    'use strict';
 
-    /* Variables */
+    // Variables.
     var express = require('express');
     var app = express();
     var server = require('http').createServer(app).listen(8080);
     var io = require('socket.io').listen(server);
     var mongoose = require('mongoose');
+    var randomString = require("randomstring");
 
-    /* Routes */
+    // Routes.
+    app.get('/', function (req, res) {
+        var gameId = randomString.generate(5);
+        res.redirect('/game/' + gameId);
+    });
     app.get('/game/:id', function (req, res) {
         res.sendfile('index.html');
     });
@@ -28,7 +33,7 @@ Author: David Clark
         res.sendfile('stats/data/moves.json');
     });
 
-    /* Routes to static files. */
+    // Routes to static files.
     app.use('/audio', express.static('audio'));
     app.use('/css', express.static('css'));
     app.use('/images', express.static('images'));
@@ -36,26 +41,35 @@ Author: David Clark
     app.use('/libraries', express.static('libraries'));
     app.use('/stats', express.static('stats'));
 
-    /* Database */
+    // Database.
     mongoose.connect('mongodb://localhost/Node-Tac-Toe');
     mongoose.connection.once('open', function callback() {
 
-        // Schema
+        // Games schema.
+        var gamesSchema = mongoose.Schema({
+            roomId: String,     // Room id of the game.
+            whoWon: Number,     // Who won.
+            winningCombo: Array,// Winning combo.
+            tieGame: Boolean    // Tie game.
+        });
+        var Games = mongoose.model('games', gamesSchema);
+
+        // Moves schema.
         var movesSchema = mongoose.Schema({
             socketId: Object,   // Socket id of the player.
             roomId: String,     // Room id of the game.
             player: Number,     // Player 1 or player 2.
             square: Number      // Square they placed.
         });
-        var moves = mongoose.model('moves', movesSchema);
+        var Moves = mongoose.model('moves', movesSchema);
 
-        // Sockets
+        // Sockets.
         io.sockets.on('connection', function (socket) {
 
             var roomId;
-            var history = [];
             var player1History = [];
             var player2History = [];
+            var totalMoves;
             var winningCombo;
             var winningCombinations = [
                 [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
@@ -66,42 +80,57 @@ Author: David Clark
             var player1Id;
             var player2Id;
             var observer;
+            var gameStarted;
+            var whoseTurn;
+            var tieGame;
 
             // Join the clients room.
             socket.on('room', function (room) {
                 socket.join(room);
                 roomId = room;
                 console.log('Client connected to room: ' + roomId);
-            });
-
-            var room = io.sockets.clients(roomId);
-            var numOfClients = room.length;
-
-            if (numOfClients === 1) {
-                player1Id = room[0].id;
-                io.sockets.socket(player1Id).emit('player', 1);
-            }
-            if (numOfClients === 2) {
-                player2Id = room[1].id;
-                io.sockets.socket(player2Id).emit('player', 2);
-            }
-            if (numOfClients > 2) {
-                for (var i = 2; i <= numOfClients; i++) {
-                    if (room[i]) {
-                        observer = room[i].id;
-                        io.sockets.socket(observer).emit('player', 3);
+                var gameRoom = io.sockets.clients(roomId);
+                var numOfClients = gameRoom.length;
+                if (numOfClients === 1) {
+                    player1Id = gameRoom[0].id;
+                    io.sockets.socket(player1Id).emit('player', 1);
+                }
+                if (numOfClients === 2) {
+                    player2Id = gameRoom[1].id;
+                    io.sockets.socket(player2Id).emit('player', 2);
+                    io.sockets.in(roomId).emit('gameStarted', true);
+                    io.sockets.in(roomId).emit('whoseTurn', 1);
+                }
+                if (numOfClients > 2) {
+                    for (var i = 2; i <= numOfClients; i++) {
+                        if (gameRoom[i]) {
+                            observer = gameRoom[i].id;
+                            io.sockets.socket(observer).emit('player', 3);
+                        }
                     }
                 }
-            }
-            
-            // Emit to opponent.
-            //io.sockets.in(roomId).emit('update', data);
+            });
+
+            // Let the observer know whose turn it is and if there is a winningcombo.
+            socket.on('observerHistoryRequest', function (data) {
+                var gameHistory = [];
+                var roomHistory = Moves.find({'roomId': roomId});
+                roomHistory.exec(function (error, roomHistory) {
+                    if (error) {
+                        console.log('Save error: ' + error);
+                    } else {
+                        for (var i = 0; i < roomHistory.length; i++) {
+                            gameHistory.push(roomHistory[i].square);
+                        }
+                        io.sockets.socket(observer).emit('gameHistory', gameHistory);
+                    }
+                });
+            });
 
             // On move.
             socket.on('move', function (data) {
 
-                // Prepare the data.
-                var move = new moves({
+                var move = new Moves({
                     socketId: socket.id,
                     roomId: roomId,
                     player: data.player,
@@ -110,11 +139,9 @@ Author: David Clark
 
                 // Save it.
                 move.save(function (error, move) {
-
                     var piece;
-
                     if (error) {
-                        console.log('Save error: ', err);
+                        console.log('Save error: ', error);
                     } else {
                         if (move.player === 1) {
                             piece = 'X';
@@ -132,16 +159,11 @@ Author: David Clark
                     }
                 });
 
-                // Store move in global history.
-                history.push({player: data.player, square: data.square});
-
                 // Store move in player history.
                 if (data.player === 1) {
                     player1History.push(data.square);
-                    console.log(player1History);
                 } else {
                     player2History.push(data.square);
-                    console.log(player2History);
                 }
 
                 // Check win state.
@@ -157,17 +179,59 @@ Author: David Clark
                     if (player1Match.length === 3) { winningCombo = player1Match; }
                     if (player2Match.length === 3) { winningCombo = player2Match; }
                     if ((player1Match.length === 3) || (player2Match.length === 3)) {
-                        if (data.player === 1) {
-                            console.log("Player 1 won.");
-                        } else {
-                            console.log("Player 2 won.");
-                        }
-                        console.log(winningCombo);
+                        io.sockets.in(roomId).emit('winStatus', {'player': data.player, 'winningCombo': winningCombo});
+                        tieGame = false;
+                        var game = new Games({
+                            roomId: roomId,
+                            whoWon: data.player,
+                            winningCombo: winningCombo,
+                            tieGame: tieGame
+                        });
+                        game.save(function (error, game) {
+                            if (error) {
+                                console.log('Save error: ', error);
+                            } else {
+                                console.log('In room ' + game.roomId + ' player ' + game.whoWon + ' won with the combo ' + game.winningCombo + '.');
+                            }
+                        });
                     }
                 }
 
+                var roomHistory = Moves.find({'roomId': roomId});
+                roomHistory.exec(function (error, roomHistory) {
+                    totalMoves = roomHistory.length + 1;
+                    if (error) {
+                        console.log('Save error: ' + error);
+                    } else {
+                        console.log(totalMoves + ' moves have been made in room ' + roomId);
+                        if (totalMoves === 9) {
+                            io.sockets.in(roomId).emit('winStatus', 'tie');
+                            var game = new Games({
+                                roomId: roomId,
+                                whoWon: 3,
+                                winningCombo: [],
+                                tieGame: true
+                            });
+                            game.save(function (error, game) {
+                                if (error) {
+                                    console.log('Save error: ', error);
+                                } else {
+                                    console.log('In room ' + game.roomId + ' a tie game.');
+                                }
+                            });
+                        }
+                    }
+                });
+
                 // Emit to opponent.
                 io.sockets.in(roomId).emit('update', data);
+
+                // Change whose turn it is.
+                if (move.player === 1) {
+                    io.sockets.in(roomId).emit('whoseTurn', 2);
+                } else {
+                    io.sockets.in(roomId).emit('whoseTurn', 1);
+                }
 
             });
 
